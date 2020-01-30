19a4469819a5d560c75f39367f86bf243c4ac7cd
Sam Odum <sam.odum1@gmail.com>
2020-01-30 19:20:35 +0100

    Modify folder structure

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -1,22 +1,22 @@
-const postsRepository = require('../repository/postsRepository');
+const postsHandler = require('../handlers/postsHandler');
 // get all the posts
 function getAll(req, res) {
-  return postsRepository.getAll(req, res);
+  return postsHandler.getAll(req, res);
 }
 
 // save a new post
 function save(req, res) {
-  return postsRepository.save(req, res);
+  return postsHandler.save(req, res);
 }
 
 // get post by id
 function getById(req, res) {
-  return postsRepository.getById(req, res);
+  return postsHandler.getById(req, res);
 }
 
 // delete post by id
 function deleteById(req, res) {
-  return postsRepository.deleteById(req, res);
+  return postsHandler.deleteById(req, res);
 }
 
 module.exports = {
